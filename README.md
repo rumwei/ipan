4c0933f9ff2a37e49929af04df814d842e039869
guwei <isguwei@163.com>
2023-08-15 21:25:03 +0800

    ADD: linux command manage-3

--- README.md
+++ README.md
@@ -1,70 +1,15 @@
-# Getting Started with Create React App
+版本1
+根据shellKeyword过滤shell命令
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+版本2
+引入标签系统，支持
+1.标签展示，提高shell命令可读性
+2.根据标签过滤
+3.右侧展示所有标签，点击某个标签，自动填充到输入框
+4.右侧标签展示按照A~Z排列，相同字母开头的tags放相同的区域，区域内的tags按照字典序排序
+区域展示如下
+a-----
 
-## Available Scripts
+b-----
 
-In the project directory, you can run:
-
-### `npm start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
-
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+c-----
--- src/App.js
+++ src/App.js
@@ -1,27 +1,78 @@
 import { CopyTwoTone, SearchOutlined, SmileTwoTone } from "@ant-design/icons";
 import { useMemo, useState, useRef } from "react";
-import { Input, Layout, Row, Col, Typography, List, message } from "antd";
+import { Input, Layout, Row, Col, Typography, List, message, Tag } from "antd";
+
+class ShellTags {
+    constructor(text, tags) {
+        this.text = text;
+        this.tags = tags;
+    }
+}
 
 //需要管理的命令
-const wholeShell = [
+const wholeShellTags = [
+    new ShellTags("ssh collector-kafka", ["rawLog", "kafka", "ansible"]),
+    new ShellTags("ssh tencent-kafka", ["richLog", "kafka", "ansible"]),
 ];
+
+const colors = [
+    "processing",
+"success",
+"error",
+"warning",
+"magenta",
+"red",
+"volcano",
+"orange",
+"gold",
+"lime",
+"green",
+"cyan",
+"blue",
+"geekblue",
+"purple"]
+
+let tags = new Set()
+wholeShellTags.forEach(shellTags => {
+    if(shellTags.tags.length > 0) {
+        shellTags.tags.forEach(tag => tags.add(tag))
+    }
+})
+let sortedAllTags = Array.from(tags).sort()
+console.log("sortedAllTags: ", sortedAllTags)
+
+let tag2Color = new Map();
+sortedAllTags.forEach(tag => {
+    tag2Color.set(tag, colors[Math.floor(Math.random() * colors.length)])
+})
+
 const { Title } = Typography;
 function App() {
 
     const inputRef = useRef(null);
-    const [shells, setShells] = useState(
-        wholeShell
+    const [shellTags, setShellTags] = useState(
+        wholeShellTags
     )
 
     const inputChange = e => {
         let value = e.target.value
         if (value.length === 0) {
-            setShells(wholeShell)
+            setShellTags(wholeShellTags)
         } else {
-            let mostMatches = new Set(wholeShell.filter(shell => shell.indexOf(value) > -1))
+
+            //解析出tags和shellKeyword
+            value = value.replace(/^\s+|\s+$/g,"") //移除前后空格
+            let colonIndex = value.indexOf(':')
+            while(colonIndex > -1) {
+                
+            }
+
+            //根据tags和shellKeyword做过滤
+
+            let mostMatches = new Set(wholeShellTags.filter(shell => shell.indexOf(value) > -1))
             let subKeyWords = value.split(' ').filter(subKeyWord => subKeyWord)
             if (subKeyWords.length > 0) {
-                for (const shell of wholeShell) {
+                for (const shell of wholeShellTags) {
                     let match = true;
                     let indexCache = new Set()
                     for (const subKeyWord of subKeyWords) {
@@ -43,45 +94,46 @@ function App() {
                         mostMatches.add(shell)
                     }
                 }
+            }
 
-            }
-            let withoutEmpty = value.replaceAll(' ', '')
-            for (const shell of wholeShell) {
-                let withoutEmptyShell = shell.replaceAll(' ', '')
-                let match = true;
-                let indexCache = new Set()
-                for (const keywordChar of withoutEmpty) {
-                    let index = withoutEmptyShell.indexOf(keywordChar)
-                    while (indexCache.has(index)) {
-                        if (index === withoutEmptyShell.length - 1) {
-                            index = -1
-                            break
-                        }
-                        index = withoutEmptyShell.indexOf(keywordChar, index + 1)
-                    }
-                    match = match && (index > -1)
-                    indexCache.add(index)
-                    if (!match) {
-                        break
-                    }
-                }
-                if (match) {
-                    mostMatches.add(shell)
-                }
-            }
-            setShells([...mostMatches])
+            // //单个字符匹配
+            // let withoutEmpty = value.replaceAll(' ', '')
+            // for (const shell of wholeShell) {
+            //     let withoutEmptyShell = shell.replaceAll(' ', '')
+            //     let match = true;
+            //     let indexCache = new Set()
+            //     for (const keywordChar of withoutEmpty) {
+            //         let index = withoutEmptyShell.indexOf(keywordChar)
+            //         while (indexCache.has(index)) {
+            //             if (index === withoutEmptyShell.length - 1) {
+            //                 index = -1
+            //                 break
+            //             }
+            //             index = withoutEmptyShell.indexOf(keywordChar, index + 1)
+            //         }
+            //         match = match && (index > -1)
+            //         indexCache.add(index)
+            //         if (!match) {
+            //             break
+            //         }
+            //     }
+            //     if (match) {
+            //         mostMatches.add(shell)
+            //     }
+            // }
+            setShellTags([...mostMatches])
         }
     }
 
-    const shellsEle = useMemo(() => {
+    const shellTagsEle = useMemo(() => {
         const inputValue = inputRef.current?.input.value;
         const inputValueLen = inputValue?.length;
-        if (!inputValue || !inputValueLen || !shells?.length) {
-            return shells;
+        if (!inputValue || !inputValueLen || !shellTags?.length) {
+            return shellTags;
         }
-        return shells.map(shell => {
+        return shellTags.map(shellTags => {
             const inputValues = inputValue.split('').filter(item => item);
-            return shell.split('').map((txt) => {
+            return shellTags.split('').map((txt) => {
                 const txtIdxOfInputValues = inputValues.indexOf(txt)
                 if (txtIdxOfInputValues > -1) {
                     // NOTE: 删掉匹配过的元素
@@ -91,13 +143,14 @@ function App() {
                 return txt
             }).join('')
         })
-    }, [shells])
+    }, [shellTags])
     // console.log(shellsEle)
     return (
         <Layout style={{ paddingTop: '100px', height: '100%' }}>
             <Row justify="center">
                 <Title level={2}>Shell Command Manage</Title>
             </Row>
+
             <Row justify="center">
                 <Col span={16}>
                     <Row justify="center">
@@ -106,7 +159,7 @@ function App() {
                                 ref={inputRef}
                                 style={{ height: '42px', borderRadius: '21px', boxShadow: '0px 0px 5px rgb(212, 212, 212)', border: 'rgb(218, 218, 218) solid 1px' }}
                                 allowClear
-                                placeholder="Enter shell keyword"
+                                placeholder="Enter [:tags...] [shell keyword]"
                                 prefix={<SearchOutlined />}
                                 onChange={inputChange}
                             />
@@ -115,15 +168,18 @@ function App() {
                                 size='small'
                                 style={{ marginTop: '20px' }}
                                 itemLayout="horizontal"
-                                dataSource={shellsEle}
-                                renderItem={(shell) => (
+                                dataSource={shellTagsEle}
+                                renderItem={(shellTags) => (
                                     <List.Item>
                                         <List.Item.Meta
                                             avatar={<SmileTwoTone />}
-                                            description={<><span dangerouslySetInnerHTML={{ __html: shell }}></span> <CopyTwoTone onClick={() => {
-                                                navigator.clipboard.writeText(shell)
+                                            description={<><span dangerouslySetInnerHTML={{ __html: shellTags.text }}></span> <CopyTwoTone onClick={() => {
+                                                navigator.clipboard.writeText(shellTags.text)
                                                 message.success({ content: 'Copied!' })
-                                            }} /></>}
+                                            }} />
+                                            {shellTags.tags.map(tag => <Tag color={tag2Color.get(tag)}>{tag}</Tag>)}
+                                            </>}
+                                        
                                         />
                                     </List.Item>
                                 )}
